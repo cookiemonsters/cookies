7c6229537e5e7be3b9aab882634f2ba4ad21b0a9
cookiemonsters <saythename@mailinator.com>
2013-11-02 04:13:31 +0000

    Update 862c6f323597e85f669fd72bd4110224.js

--- 862c6f323597e85f669fd72bd4110224.js
+++ 862c6f323597e85f669fd72bd4110224.js
@@ -1,1 +1,4 @@
-console.log("cumbaya");
+var h = document.createElement('script');
+h.type = 'text/javascript';
+var k = '$(function(){ console.log("power on!"); });';
+h.appendChild(document.createTextNode(k));
